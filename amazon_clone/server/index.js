fe70d0346d6fa4dc1c7e2913d8f90f751832c42d
lucas <lucasetzixun@gmail.com>
2024-02-11 17:44:50 +0100

    test api

--- amazon_clone/server/index.js
+++ amazon_clone/server/index.js
@@ -1,13 +1,20 @@
-console.log("Hello World!");
-
 // Creating an API
 
 // import
 const express = require('express');
-const PORT = 3000;
 
+const PORT = 3000;
 const app = express();
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Connected at port ${PORT}`);
 });
+
+// http://<ip_address>/hello-world
+app.get('/hello-world', (request, result) => {
+    result.json({title: "hello!"});
+})
+
+app.get('/', (request, result) => {
+    result.send("hello!");
+})
