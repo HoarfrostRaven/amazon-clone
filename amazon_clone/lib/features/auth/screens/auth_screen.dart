d7290ef336e697b0dea900135bb7a667935ebcf0
lucas <lucasetzixun@gmail.com>
2024-02-11 17:17:51 +0100

    test node js

--- amazon_clone/lib/features/auth/screens/auth_screen.dart
+++ amazon_clone/lib/features/auth/screens/auth_screen.dart
@@ -44,7 +44,9 @@ class _AuthScreenState extends State<AuthScreen> {
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
               ),
               ListTile(
-                tileColor: _auth == Auth.signup ? GlobalVariables.backgroundColor : GlobalVariables.greyBackgroundColor,
+                tileColor: _auth == Auth.signup
+                    ? GlobalVariables.backgroundColor
+                    : GlobalVariables.greyBackgroundColor,
                 title: const Text(
                   'Create Account',
                   style: TextStyle(fontWeight: FontWeight.bold),
@@ -71,10 +73,12 @@ class _AuthScreenState extends State<AuthScreen> {
                           controller: _nameController,
                           hintText: 'Name',
                         ),
+                        const SizedBox(height: 10),
                         CustomTextField(
                           controller: _emailController,
                           hintText: 'Email',
                         ),
+                        const SizedBox(height: 10),
                         CustomTextField(
                           controller: _passwordController,
                           hintText: 'Password',
@@ -86,6 +90,9 @@ class _AuthScreenState extends State<AuthScreen> {
                   ),
                 ),
               ListTile(
+                tileColor: _auth == Auth.signin
+                    ? GlobalVariables.backgroundColor
+                    : GlobalVariables.greyBackgroundColor,
                 title: const Text(
                   'Sign in',
                   style: TextStyle(fontWeight: FontWeight.bold),
@@ -99,7 +106,30 @@ class _AuthScreenState extends State<AuthScreen> {
                         _auth = val!;
                       });
                     }),
-              )
+              ),
+              if (_auth == Auth.signin)
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  color: GlobalVariables.backgroundColor,
+                  child: Form(
+                    key: _signInFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomButton(text: "Sign In", onTap: () {})
+                      ],
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
---
+++ amazon_clone/server/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+
+// Creating an API
+
+// import
+const express = require('express');
+const PORT = 3000;
+
+const app = express();
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Connected at port ${PORT}`);
+});
