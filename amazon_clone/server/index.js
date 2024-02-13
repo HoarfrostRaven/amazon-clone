b3c750326d59e9d915c758450b769dc32ef187c9
lucas <lucasetzixun@gmail.com>
2024-02-13 20:50:46 +0100

    before connecting to the db

--- amazon_clone/server/index.js
+++ amazon_clone/server/index.js
@@ -1,5 +1,6 @@
 // IMPORT FROM PACKAGES
 const express = require('express');
+const mongoose = require('mongoose');
 
 // IMPORT FROM OTHER FILES
 const authRouter = require('./routes/auth')
@@ -10,6 +11,15 @@ const app = express();
 
 // middle ware
 app.use(authRouter);
+
+// Connections
+mongoose.connect()
+    .then(() => {
+        console.log(`Connected to the database.`)
+    })
+    .catch((e) => {
+        console.log(e);
+    });
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Connected at port ${PORT}`);
